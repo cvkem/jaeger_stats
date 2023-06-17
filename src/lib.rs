1ae80539c1f7b501eababaac9030efecd0fbd45e
Cees van Kemenade <cees.van.kemenade@gmail.com>
2023-06-17 20:55:11 +0200

    Rename 'mod trace' to 'mod process_map'

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 
 mod data;
 mod read;
-mod trace;
+mod process_map;
 
 
 pub use data::{
     JaegerTrace,
     JaegerItem};
 pub use read::read_jaeger_trace_file;
-pub use trace::test_trace;
+pub use process_map::test_trace;
