1590a511510c72390e97b1da4f696a5dacd6348e
Cees van Kemenade <cees.van.kemenade@gmail.com>
2023-06-22 06:20:15 +0200

    Amended statistics

--- src/stats.rs
+++ src/stats.rs
@@ -19,14 +19,16 @@ impl PathStats {
 
 #[derive(Debug, Default)]
 pub struct Stats {
-    count: usize,
+//    count: usize,
+    num_received_calls: usize,    // inbound calls to this process
+    num_outbound_calls: usize,  // outbound calls to other processes
     method: HashMap<String, usize>,
     call_chain: HashMap<String, PathStats>,
 }
 
 impl Stats {
     pub fn new() -> Self {
-        Stats{count: 0, method: HashMap::new(), call_chain: HashMap::new()}
+        Stats{num_received_calls: 0, num_outbound_calls: 0, method: HashMap::new(), call_chain: HashMap::new()}
     }
 }
 
@@ -46,6 +48,8 @@ impl StatsMap {
 
         let spans = &trace.spans;
 
+        //TODO:: collect some basics
+
         spans
             .iter()
             .enumerate()
@@ -56,7 +60,10 @@ impl StatsMap {
                 };
                 let method = &span.operation_name;
                 let update_stat = |stat: &mut Stats| {
-                    stat.count += 1;
+                    match &method[..] {
+                        "GET" | "POST" | "HEAD" | "QUERY" => stat.num_outbound_calls += 1,
+                        _ => stat.num_received_calls += 1 
+                    }
 
                     // add a count per method
                     stat.method
@@ -73,7 +80,7 @@ impl StatsMap {
                     println!(" get call_chain_str");
                     let call_chain_str = call_chain
                         .into_iter()
-                        .fold(String::new(), |a, b| a + &b + " | ");
+                        .fold(String::new(), |a, b| a + &b.0 + "/" + &b.1 + " | ");
                     stat.call_chain
                         .entry(call_chain_str)
                         .and_modify(|ps| ps.count += 1)
@@ -95,13 +102,15 @@ impl StatsMap {
     pub fn to_csv_string(&self) -> String {
         let mut s = Vec::new();
 
+        //TODO: add generic stats
+
         let mut data: Vec<_> = self.0.iter().collect();
         data.sort_by(|a,b| { a.0.cmp(&b.0)});
 
-        s.push("process; count".to_owned());
+        s.push("process; num_received_calls; num_outbound_calls".to_owned());
         data.iter()
             .for_each(|(k, stat)| {
-                let line = format!("{k};{}", stat.count);
+                let line = format!("{k};{};{}", stat.num_received_calls, stat.num_outbound_calls);
                 s.push(line);
             });
         s.push("\n".to_owned());
@@ -155,32 +164,32 @@ pub fn basic_stats(trace: &Trace) -> HashMap<String, u32> {
 }
 
 
-fn get_parent_processes(parent_idx: Option<usize>, spans: &Spans) -> String {
-    let mut parents = Vec::new();
-    let mut par_idx = parent_idx;
-    loop {
-        match par_idx {
-            None => break,
-            Some(idx) => {
-                let par_span = &spans[idx];
-                parents.push(match &par_span.process {
-                    Some(p) => p.name.to_owned(),
-                    None => "-".to_owned()
-                });
-                par_idx = par_span.parent;
-            }
-        }
-    }
-    let res = parents
-        .into_iter()
-        .rev()
-        .fold(String::new(), |a, b| a + &b + "|");
-    res
-}
+// fn get_parent_processes(parent_idx: Option<usize>, spans: &Spans) -> String {
+//     let mut parents = Vec::new();
+//     let mut par_idx = parent_idx;
+//     loop {
+//         match par_idx {
+//             None => break,
+//             Some(idx) => {
+//                 let par_span = &spans[idx];
+//                 parents.push(match &par_span.process {
+//                     Some(p) => p.name.to_owned(),
+//                     None => "-".to_owned()
+//                 });
+//                 par_idx = par_span.parent;
+//             }
+//         }
+//     }
+//     let res = parents
+//         .into_iter()
+//         .rev()
+//         .fold(String::new(), |a, b| a + &b + "|");
+//     res
+// }
 
 
-/// parent_call_chain returns a Vec<String> that shows the full call_chain from top to bottom
-fn get_call_chain(idx: usize, spans: &Spans) -> Vec<String> {
+/// parent_call_chain returns the full call_chain from top to bottom showing process and called method
+fn get_call_chain(idx: usize, spans: &Spans) -> Vec<(String, String)> {
     let span = &spans[idx];
     // find the root and allocate vector
     let mut call_chain = match span.parent {
@@ -188,24 +197,26 @@ fn get_call_chain(idx: usize, spans: &Spans) -> Vec<String> {
          Some(idx) => get_call_chain(idx, spans)
         };
     // and push all proces names starting from the root
-    call_chain.push(match &span.process {
+    let process = match &span.process {
         Some(p) => p.name.to_owned(),
         None => "-".to_owned()
-    });
+    };
+    let method = span.operation_name.to_owned();
+    call_chain.push( (process, method) );
     call_chain
 }
 
 /// get all values that appear more than once in the list of strings, while being none-adjacent.
-fn get_duplicates(names: &Vec<String>) -> Vec<String> {
+fn get_duplicates(names: &Vec<(String, String)>) -> Vec<String> {
     let mut duplicates = Vec::new();
     for idx in 0..names.len() {
-        let nme = &names[idx];
+        let proc = &names[idx].0;
         let mut j = 0;
         loop {
             if j >= duplicates.len() {
                 break;
             }
-            if duplicates[j] == *nme {
+            if duplicates[j] == *proc {
                 break;
             }
             j += 1;
@@ -216,13 +227,13 @@ fn get_duplicates(names: &Vec<String>) -> Vec<String> {
         //  nme does not exist in duplicates yet, so find it in names
         let mut j = idx + 2; // Step by 2 as we want to prevent matching sub-sequent GET calls
         loop {
-            if j>= names.len() || names[j] == *nme {
+            if j>= names.len() || names[j].0 == *proc {
                 break;
             }
             j += 1;
         }
         if j < names.len() {
-            duplicates.push(nme.to_owned());
+            duplicates.push(proc.to_owned());
         }
     }
     duplicates
@@ -243,7 +254,7 @@ pub fn chained_stats(trace: &Trace) -> HashMap<String, u32> {
             };
             let parents_str = get_call_chain(idx, spans)
                 .into_iter()
-                .fold(String::new(), |a, b| a + &b + "|");
+                .fold(String::new(), |a, b| a + &b.0 + &b.1 +" | ");
             let proc_method = format!("{parents_str}{proc}/{}", span.operation_name);
             stats.entry(proc_method).and_modify(|counter| *counter += 1).or_insert(1);
         });
