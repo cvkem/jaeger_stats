c4a3a2c703093865844c29ccc4b8d1c78651b815
Cees van Kemenade <cees.van.kemenade@gmail.com>
2023-07-19 15:22:26 +0200

    Deduplication of traces added

--- src/analyse.rs
+++ src/analyse.rs
@@ -10,7 +10,7 @@ use crate::{
         TraceExt,
         write_stats_to_csv_file, write_string_to_file}};
 use std::{
-    collections::HashMap,
+    collections::{HashMap, HashSet},
     error::Error,
     ffi::OsStr,
     fs,
@@ -71,6 +71,35 @@ fn extend_create_folder(folder: &PathBuf, subfolder: &str) -> PathBuf {
     ext_folder
 }
 
+
+/// deduplicate all the traces based on traceId and report effect,
+fn deduplicate(traces: Vec<TraceExt>) -> Vec<TraceExt> {
+    let initial_num = traces.len();
+
+    let mut observed_id = HashSet::new();
+    let mut duplicated_ids = Vec::new();
+
+    let traces: Vec<_> = traces.into_iter()
+        .filter(|tr| {
+            let trace_id = &tr.trace.trace_id;
+            if observed_id.insert(trace_id.clone()) {
+                true  // this is a new trace_id
+            } else {
+                duplicated_ids.push(trace_id.clone());
+                false
+            }
+        })
+        .collect();
+
+    let num_duplicates = duplicated_ids.len();
+    let remaining = traces.len();
+    report(Chapter::Summary, format!("Removed {num_duplicates}:  So list of {initial_num} traces reduced to {remaining}"));
+    report(Chapter::Details, format!("Removed duplicates: {duplicated_ids:?}"));
+
+    traces
+}
+
+
 /// process a vector of traces
 fn process_traces(folder: PathBuf, traces: Vec<Trace>, caching_processes: Vec<String>, cchain_cache: &mut CChainEndPointCache) {
 
@@ -87,6 +116,9 @@ fn process_traces(folder: PathBuf, traces: Vec<Trace>, caching_processes: Vec<St
             .map(|trace| TraceExt::new(trace, &trace_folder, &caching_processes))
             .collect::<Vec<_>>()
     };
+
+    let traces = deduplicate(traces);
+
     //println!("Now writing all traces");
     traces.iter().for_each(|trace| trace.write_trace());
 
